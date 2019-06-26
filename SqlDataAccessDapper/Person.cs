fda10ba8362d27eb966d4b8988f1c3b799112692
Tomasz Plewniak <tomekplewniak@gmail.com>
2019-06-26 14:25:47 +0200

    Add MainWindow and Person class.

---
+++ SqlDataAccessDapper/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlDataAccessDapper
+{
+    class Person
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private string dwadwa;
+
+        public string FullInfo
+        {
+            get
+            {
+                return $"{FirstName} {LastName} ({EmailAddress})";
+            }
+        }
+    }
+}
